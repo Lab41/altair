1f1035425202398326cfa845cbdcc927644aa7e8
kylemvz <kylez@lab41.org>
2017-01-24 14:26:19 -0800

    Reorganize to conform to altair package folder hierarchy.

---
+++ altair/main.py
@@ -0,0 +1,7 @@
+from altair.preprocess00 import separate_code_and_comments
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
---
+++ altair/vectorize01/comment/CommentVectorizer.py
@@ -0,0 +1,6 @@
+class CommentVectorizer:
+    def __init__(self):
+        pass
+
+    def vectorize(self, comment):
+        return comment
